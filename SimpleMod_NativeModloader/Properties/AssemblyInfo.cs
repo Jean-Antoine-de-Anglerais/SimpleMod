7cf1db42f0d25432c470af3d5651ec6a7ed4381e
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-10-22 21:21:18 +0300

    Добавьте файлы проекта.

---
+++ SimpleMod_NativeModloader/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("SimpleMod_NativeModloader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleMod_NativeModloader")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e6ea5249-6775-4b96-ac60-e7e82f9cf204")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleMod_NativeModloader/WorldBoxMod.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace SimpleMod_NativeModloader
+{
+    public class WorldBoxMod : MonoBehaviour
+    {
+        static bool initialized = false;
+
+        void Update()
+        {
+            if (!Config.gameLoaded) return;
+
+
+            if (!initialized)
+            {
+                init();
+            }
+
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                MapBox.instance.selectedButtons.selectedButton.godPower = AssetManager.powers.get("super_skeleton");
+            }
+
+            initialized = true;
+        }
+
+        void init()
+        {
+            SimpleMod simpleMod = new SimpleMod();
+
+            GodPower power = AssetManager.powers.clone("super_skeleton", SA.skeleton);
+            power.name = "Super Skeleton";
+            power.actor_asset_id = "super_skeleton";
+            power.click_action = new PowerActionWithID(AssetManager.powers.spawnUnit);
+
+            LocalizedTextManager.instance.localizedText.Add(power.name, power.name);
+        }
+    }
+}
